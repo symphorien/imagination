2aed63e915ffd239a624235ea1c68bf812fd2b15
Symphorien Gibol <symphorien+git@xlumurb.eu>
2019-08-12 16:56:30 +0200

    do not attract attention of user on available encoder
    Because of this the "message" tab is always in bold, which defeats the
    purpose of setting it in bold *when attention is needed*.

--- src/support.c
+++ src/support.c
@@ -1143,12 +1143,12 @@ img_check_for_encoder(img_window_struct *img)
 	name = g_find_program_in_path("ffmpeg");
 	if (name == NULL)
 	{
-		img_message(img, TRUE, "Using avconv for encoding.\n");
+		img_message(img, FALSE, "Using avconv for encoding.\n");
 		img->encoder_name = g_strdup("avconv");
 	}
 	else
 	{
-		img_message(img, TRUE, "Using ffmpeg for encoding.\n");
+		img_message(img, FALSE, "Using ffmpeg for encoding.\n");
 		img->encoder_name = name;
 	} 
 }
