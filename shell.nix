d7f90a349e340f0c8d65c747ec09240f8e923f7b
gt67 <gt67@6247ab75-7297-438c-a817-b4e4d5db8f37>
2019-12-14 08:35:08 +0000

    add shell.nix
    git-svn-id: https://svn.code.sf.net/p/imagination/code/trunk@681 6247ab75-7297-438c-a817-b4e4d5db8f37

---
+++ shell.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs> {};
+mkShell {
+  nativeBuildInputs = [ 
+  bear subversion ffmpeg sox autoreconfHook intltool libxslt.bin docbook_xml_xslt libtool pkg-config ];
+  buildInputs = map enableDebugging [ glib gtk3 ];
+  checkInputs = [
+    (python3.withPackages (ps: [ ps.dogtail ]))
+  gnome3.gobject-introspection at-spi2-atk at-spi2-core
+  tesseract imagemagick
+  ];
+  doCheck = true;
+  shellHook = ''
+    export XDG_DATA_DIRS=${gtk3}/share/gsettings-schemas/${gtk3.name}:${gsettings-desktop-schemas}/share/gsettings-schemas/${gnome3.gsettings-desktop-schemas.name}:${hicolor-icon-theme}/share:${gnome3.adwaita-icon-theme}/share:$XDG_DATA_DIRS
+  '';
+  G_DEBUG="fatal-criticals";
+}
